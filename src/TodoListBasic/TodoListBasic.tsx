dd2553455c93ac65eeac1309d251d65c392b9002
Made-In-Heaven-D4C-Love-Train <90892596+Made-In-Heaven-D4C-Love-Train@users.noreply.github.com>
2023-01-23 10:56:55 +0100

    Add files via upload

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import "./TodoListBasic.css";
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
+    
+   const [toDo, setToDo] = useState<string>("");
+   const [inProgress, setInProgress] = useState<string>("");
+   const [done, setDone] = useState<string>("");
+   const [toDoList, setToDoList] = useState<string[]>([]);
+   const [inProgressList, setInProgressList] = useState<string[]>([]);
+   const [DoneList, setDoneList] = useState<string[]>([]);
+
+   const handleAddToDo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToDo(e.target.value);
+   }
+   const handleClickToDo = () => {
+    setToDoList(prev => [...prev, toDo])
+};
+
+const handleAddInProgress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInProgress(e.target.value);
+   }
+   const handleClickInProgress = () => {
+    setInProgressList(prev => [...prev, inProgress])
+};
+
+const handleAddDone = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDone(e.target.value);
+   }
+   const handleClickDone = () => {
+    setDoneList(prev => [...prev, done])
+};
+    return <>
+    
+    <label>To do :</label>
+    <input type="text" value={toDo} onChange={handleAddToDo} />
+    <button onClick={handleClickToDo}>Add</button><br /><br />
+
+
+    <label>In progress :</label>
+    <input type="text" value={inProgress} onChange={handleAddInProgress} />
+    <button onClick={handleClickInProgress}>Add</button><br /><br />
+
+    <label>Done :</label>
+    <input type="text" value={done} onChange={handleAddDone} />
+    <button onClick={handleClickDone}>Add</button><br /><br />
+
+
+    <p>To do :</p>
+    <ul>
+        {toDoList.map((toDo, index) => (<li key={index}>{toDo}</li>))}
+    </ul>
+    <p>In progress :</p>
+    <ul>
+        {inProgressList.map((inProgress, index) => (<li key={index}>{inProgress}</li>))}
+    </ul>
+    <p>Done:</p>
+    <ul>
+        {DoneList.map((done, index) => (<li key={index}>{done}</li>))}
+    </ul>
+
+    </>
 };
 
 export default TodoListBasic;
